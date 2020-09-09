95b96caaac230ad31714d2bb8bac7d2222ad000b
yskim-web <abcm556@naver.com>
2020-09-09 14:39:07 +0900

    test5 done

--- python0909/Test05.py
+++ python0909/Test05.py
@@ -20,3 +20,16 @@ print(date[0:4]+"년"+date[5:7]+"월"+date[8:]+"일")
 str = "Global IT"
 str = str.replace("IT","institute")
 print(str)
+
+#문자열 나누기 : split()
+a,b,c,d,e = "1 2 3 4 5".split();#공백을 기준으로 나눔
+print(a)
+
+str1 = "Global:it"
+a,b = str1.split(":")
+print(a)
+print(b)
+
+#문자열 길이 : len()
+str="wetwet234t13dsadeefaefwegwgwerewrafdsf"
+print(len(str))
