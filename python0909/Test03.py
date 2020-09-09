f7d14ee21db3e215b6165286edcb5c7dba90c26f
yskim-web <abcm556@naver.com>
2020-09-09 13:35:35 +0900

    test3

--- python0909/Test03.py
+++ python0909/Test03.py
@@ -9,6 +9,35 @@
     정수,실수 >> 문자
         str(값)
 '''
-age = input("나이을 입력하세요.")
-age = int(age)
-print(age+10)
+
+#age = input("나이을 입력하세요.")
+#age = int(age)
+#print(age+10)
+
+#문자열 str
+#"문자열" '문자열' """여러문자열""" '''여러문자열'''
+print("""hello
+python
+hahahah""")
+
+#문자열 연산
+a = "pyton"
+b = "더하기"
+c = a+b
+print(c)
+print(a+b)
+print(a,b)
+
+#2. 문자열 반복하기 : *
+print(a*10)
+d = a*10
+print(d)
+
+#실습
+person1 = int(input("입력1"))
+person2 = int(input("입력2"))
+person3 = int(input("입력3"))
+result = (person1+person2+person3)/3
+print("결과는 %.1f입니다" %result)
+
+
