034970c99de3f7fadf386fc6989ed03c69f268f9
yskim-web <abcm556@naver.com>
2020-09-09 14:03:35 +0900

    test4 done

--- python0909/Test04.py
+++ python0909/Test04.py
@@ -3,3 +3,11 @@ a = "Global Institute"
 print(a[0:6])
 print(a[:6])
 print(a[7:16])
+print(a[:])
+
+str = "prigraming"
+print(str)
+str = str[0:2]+'o'+str[3:7]+"m"+str[7:]
+print(str)
+str = "programming"
+print(str)
