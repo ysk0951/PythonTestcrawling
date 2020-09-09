c5761642af444f967a8029eae4b1e59e48a8dc61
yskim-web <abcm556@naver.com>
2020-09-09 12:00:27 +0900

    test2 done

---
+++ python0909/Test02.py
@@ -0,0 +1,33 @@
+'''
+python data type
+    숫자형
+        정수 : int(python 3부터 long과 int 통합)
+        실수 : float
+    논리형
+        bool : True / False
+    군집자료형
+        문자열 : str
+        리스트 : list
+        튜플 : tuple
+        딕셔너리 : dict
+        집합 : set
+# 변수 variable
+    변수 = 값
+    동적언어 : 변수에 저장되는 언어를 스스로 판단하여 그 종류를 알아냄
+    데이터 타입 나타내는 메소드 : type()
+'''
+num = 10
+num2 = 10.2
+print(num);
+print(type(num))
+print(type(num2))
+num3 = 'hello'
+print(num3)
+num4 = True;
+print(num4)
+str = 'C'
+print(type(str))
+
+
+
+
