586e0271b98399c23e0c71c82d82766aa8cef189
yskim-web <abcm556@naver.com>
2020-09-09 14:20:18 +0900

    test6 done

--- python0909/Test05.py
+++ python0909/Test05.py
@@ -7,5 +7,16 @@
     논리 연산자 : True / False
     멤버 연산자 : in / not in
     식별 연산자 : is / is not
-
 '''
+#
+import datetime
+date = datetime.date.today();
+print(type(date))
+date = str(date)
+print(date)
+print(date[0:4]+"년"+date[5:7]+"월"+date[8:]+"일")
+
+#문자열 함수
+str = "Global IT"
+str = str.replace("IT","institute")
+print(str)
