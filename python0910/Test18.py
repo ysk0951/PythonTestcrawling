d6d537aa6353135db99859cb0028eb5261b64d2f
yskim-web <abcm556@naver.com>
2020-09-10 13:51:26 +0900

    test18 done

---
+++ python0910/Test18.py
@@ -0,0 +1,15 @@
+# 함수 : 파라미터와 인자 : 개수*와 순서가 일치해야함.
+# 파라미터의 초기값 넣어주기 : 초기값 있는 파라미터는 파라미터들중 가장 뒤에 위치해야함
+
+def signUp(username, pw, name, gender, email, addr, nation="S.Korea"):
+    print(username)
+    print(pw)
+    print(name)
+    print(gender)
+    print(email)
+    print(addr)
+    print(nation)
+
+signUp("global","1234","피카츄","mail","pika@gmail.com","관악구","Korea")
+# 파라미터와 인자 : 개수 , 순서*가 일치해야한다.
+# 인자를 기입할때, 파라미터=값
