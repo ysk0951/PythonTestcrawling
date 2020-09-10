589a568c96b526d3303c44a4852c7b8250fe5b6b
yskim-web <abcm556@naver.com>
2020-09-10 10:59:16 +0900

    test11 done

--- python0910/Test12.py
+++ python0910/Test12.py
@@ -15,8 +15,10 @@
 시험점수가 60이상이면 합격 미만은 불합격 출력
 '''
 score = [90,40,65,80,59]
+num = 1
 for i in score:
     if i >=60:
-        print("합격")
+        print("%d번 학생의 점수는 %d점으로 합격" %(num,i))
     else :
-        print("불합격")
+        print("%d번 학생의 점수는 %d점으로 불합격"  %(num,i))
+    num +=1
