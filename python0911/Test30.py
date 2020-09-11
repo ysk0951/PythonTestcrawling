9ded322ce697d8304a6a664f4639bded5b2d110b
yskim-web <abcm556@naver.com>
2020-09-11 11:51:31 +0900

    test30 done

---
+++ python0911/Test30.py
@@ -0,0 +1,15 @@
+'''
+딕셔너리 dict
+key value 쌍으로 이루어진 데이터들이 저장되는 공간
+수정가능 mutable
+구조
+    변수명 = {key:value , key:value,...}
+
+'''
+
+dic = {"a" : 10, "b":"hahaha","c":True}
+print(dic)
+print(dic["a"])
+
+dic = {}
+print(type(dic))
