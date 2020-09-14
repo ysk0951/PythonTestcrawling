31aa1238be86fe0fd5e2a5fafa2f6e35f4470736
yskim-web <abcm556@naver.com>
2020-09-14 13:49:48 +0900

    #

--- python0914/Test47.py
+++ python0914/Test47.py
@@ -13,10 +13,28 @@ except error:
     예외 처리 code
 
 '''
-print("프로그램 시작")
-while True:
-    num = input("점수 : ")
-    int(num)
+
+# print("프로그램 시작")
+# while True:
+#     try:
+#         num = input("점수 : ")
+#         int(num)
+#         print(num)
+#         break
+#     except ValueError:
+#         print("error")
+#         break
+# print("프로그램 종료")
+
+# lst = [1,2,3]
+# try :
+#     idx = int(input("인덱스 번호 입력 : "))
+#     print(lst[idx])
+# except IndexError as e:
+#     print("index Error =",end=" ")
+#     print(e)
+
+def func():
+    num =int(input("1~5 사이 정수 입력 :"))
     print(num)
-    break
-print("프로그램 종료")
+
