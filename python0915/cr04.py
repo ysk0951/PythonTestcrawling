9e85dc9c6b694988e39191c447176e0d3a1bbb14
yskim-web <abcm556@naver.com>
2020-09-15 14:54:46 +0900

    crawling

--- python0915/cr04.py
+++ python0915/cr04.py
@@ -29,28 +29,28 @@ from bs4 import BeautifulSoup
 # soup = BeautifulSoup(html,"html.parser")
 # print(soup)
 # print(type(soup))
-
-html = '''<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title class="t a b" id="tid">test Title</title>
-</head>
-<body>
-    <p>1111</p>
-    <p>2222</p>
-    <p>3333</p>
-</body>
-</html>'''
+#
+# html = '''<!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <title class="t a b" id="tid">test Title</title>
+# </head>
+# <body>
+#     <p>1111</p>
+#     <p>2222</p>
+#     <p>3333</p>
+# </body>
+# </html>'''
 #parsing
-soup = BeautifulSoup(html,'lxml')
+# soup = BeautifulSoup(html,'lxml')
 # print(soup)
 # print(type(soup))
 # print("="*40)
 
 #1. 간단한 태그 접근
-title = soup.title
-p = soup.body
+# title = soup.title
+# p = soup.body
 # print(title,                        # 태그전체
 #       "\ntype=",type(title),        # 태그의 타입
 #       "\ntext=",title.text,         # 태그의 내부텍스트와 하위태그포함
@@ -64,17 +64,20 @@ p = soup.body
 # print(t_tag['class']) # dictionary에서 key로 value꺼내기 - 없으면  Error
 # print(t_tag.get('class')) # dictionary에서 key로 value꺼내기 - 없을시 None
 
-html = '''<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title class="t a b" id="tid">test Title</title>
-</head>
-<body>
-    <p><span>1111</<span><span>2222</<span></p>
-</body>
-</html>'''
-soup = BeautifulSoup(html,'lxml')
+# html = '''<!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <title class="t a b" id="tid">test Title</title>
+# </head>
+# <body>
+#     <p>
+#           <span>1111</<span>
+#           <span>2222</<span>
+#     </p>
+# </body>
+# </html>'''
+# soup = BeautifulSoup(html,'lxml')
 # p_tag = soup.p
 #
 # text_data = p_tag.text
@@ -83,20 +86,88 @@ soup = BeautifulSoup(html,'lxml')
 # print("text : ",text_data,type(text_data)) # 자식포함
 # print("string : ",string_data,type(string_data)) # 바로밑에만 읽기
 #-----------------------------------------------------------------------------------
-p_tag_c = soup.p.contents
-print("tag_c",p_tag_c)
-
-p_tag_ch = soup.p.children
-print("tag_ch",p_tag_ch)
-for child in p_tag_ch:
-      print(child)
+# p_tag_c = soup.p.contents
+# print("tag_c",p_tag_c)
+#
+# p_tag_ch = soup.p.children
+# print("tag_ch",p_tag_ch)
+# for child in p_tag_ch:
+#       print(child)
 
 #부모태그 접근
-span_tag = soup.span
-print(span_tag)
-print(span_tag.parent)
-print("="*50)
-parents = span_tag.parents
-for p in parents:
-      print(p)
+# span_tag = soup.span
+# print(span_tag)
+# print(span_tag.parent)
+# print("="*50)
+# parents = span_tag.parents
+# for p in parents:
+#       print(p)
+
+# 형제 태그 접근 : next_sibling,previous_sibling
+# html = '''<!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <title class="t a b" id="tid">test Title</title>
+# </head>
+# <body>
+#     <p>
+#           <span>1111</<span>
+#           <span>2222</<span>
+#     </p>
+# </body>
+# </html>'''
+# soup = BeautifulSoup(html,'lxml')
+# span_tag = soup.span
+# n = span_tag.next_sibling
+# p = span_tag.previous_sibling
+# print(n)
+# print(p)
+
+# 이전 요소, 다음 요소 : next_element(s), previous_element(s)
+# html = """<head>
+#     <title>test</title>
+# </head>
+# <body>
+#     <p>
+#         <a>test1
+#             <span>123</span>
+#             <span>456</span>
+#         </a>
+#         <b>test2</b>
+#         <c>test3</c>
+#     </p>
+# </body>
+# </html>
+# """
+# soup = BeautifulSoup(html,"lxml")
+# print(soup)
+# a_tag = soup.a
+# print(a_tag)
+# s = a_tag.next_sibling
+# print(type(s))
+# for ss in s:
+#       print(ss)
+# print("===========")
+# e = a_tag.next_elements
+# print(type(e))
+# for ee in s:
+#       print(ee)
+
+'''원하는 요소 정확히 접근하기'''
+html = '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title class="t a b" id="tid">test Title</title>
+</head>
+<body>
+    <p>1111</p>
+    <p id="a">2222</p>
+    <p>3333</p>
+</body>
+</html>'''
+soup = BeautifulSoup(html,'lxml')
+print(soup.find_all('p'))     #태그명으로 가져오기
+print(soup.find_all(id='a'))  #id로 가져오기
 
