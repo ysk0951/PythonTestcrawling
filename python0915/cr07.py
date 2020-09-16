e171a83be0a07014f53fa269a227958606295f8c
yskim-web <abcm556@naver.com>
2020-09-16 10:24:58 +0900

    #

--- python0915/cr07.py
+++ python0915/cr07.py
@@ -1,4 +1,5 @@
 #네이버 뉴스기사
+from typing import List, Any
 
 import requests
 from bs4 import BeautifulSoup
@@ -25,11 +26,11 @@ res = requests.get(url)
 # for i in data :
 #     print(i.text.strip())
 
-soup = BeautifulSoup(res.content,"lxml")
-dataByday = soup.select(".title")
-for i in dataByday:
-    day = i.attrs['href'][-3:]
-    print(i.attrs['href'][-3:])
+# soup = BeautifulSoup(res.content,"lxml")
+# dataByday = soup.select(".title")
+# for i in dataByday:
+#     day = i.attrs['href'][-3:]
+#     print(i.attrs['href'][-3:])
 # print(dataByday)
 
 
@@ -38,7 +39,43 @@ for i in dataByday:
 #     print(ii)
 # print(i+1,":",data[i].text.strip())
 
-
 #네이버 미세먼지
 html = requests.get('https://search.naver.com/search.naver?query=%EB%82%A0%EC%94%A8')
-print(html.text)
+soup = BeautifulSoup(html.content,"lxml")
+# print(soup.prettify())
+# data= soup.find('dd',class_='lv1')
+# print(data.text)
+
+data = soup.find('dl',class_='indicator')
+print(data.prettify())
+dt = data.find_all('dt')
+
+for d in dt:
+    print(d.text)
+    # dt_list.append(d.text)
+
+find_dust= []
+span = data.find_all('span',{'class':'num'})
+for s in span:
+    print(s.text)
+
+dd= data.find_all('dd',class_='lv1')
+for d in dd:
+    print(d.text)
+dt_list=[]
+find_dust= []
+find_dust_status = []
+span = data.find_all('span',{'class':'num'})
+for s in span:
+    find_dust.append(s.text)
+    find_dust_status.append(s.next_sibling)
+
+for i in range(3):
+    print(dt_list[i],"=",find_dust[i],find_dust_status[i])
+
+with open('findDust.txt','w',encoding="utf-8") as file:
+    for i in range(3):
+        str = dt_list[i]+"="+find_dust(i)+find_dust_status(i)+"\n"
+        file.write(str)
+
+
