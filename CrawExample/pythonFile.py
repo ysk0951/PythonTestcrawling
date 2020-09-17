3dc787c9e9f25102db0e65c303d4146b6125218d
yskim-web <abcm556@naver.com>
2020-09-17 10:22:10 +0900

    #

--- CrawExample/pythonFile.py
+++ CrawExample/pythonFile.py
@@ -36,22 +36,22 @@ cursor = connection.cursor()
 # connection.commit()
 # connection.close()
 
-# 여러 레코드 삽입 방식2
-users = [
-    ('py1','0000',20,datetime.datetime.now()),
-    ('py2','0000',20,datetime.datetime.now()),
-    ('py3','0000',20,datetime.datetime.now()),
-    ('py4','0000',20,datetime.datetime.now())
-]
-sql = 'insert into test values(:1,:2,:3,:4)'
-
-#DB에 저장시킬 레코드의 개수 cursor에 세팅
-cursor.arraysize = len(users)
-#한방에 실행
-cursor.executemany(sql,users)
-cursor.close()
-connection.commit()
-connection.close()
+# # 여러 레코드 삽입 방식2
+# users = [
+#     ('py1','0000',20,datetime.datetime.now()),
+#     ('py2','0000',20,datetime.datetime.now()),
+#     ('py3','0000',20,datetime.datetime.now()),
+#     ('py4','0000',20,datetime.datetime.now())
+# ]
+# sql = 'insert into test values(:1,:2,:3,:4)'
+#
+# #DB에 저장시킬 레코드의 개수 cursor에 세팅
+# cursor.arraysize = len(users)
+# #한방에 실행
+# cursor.executemany(sql,users)
+# cursor.close()
+# connection.commit()
+# connection.close()
 
 # #결과물 추출
 # query = 'select * from test'
@@ -59,8 +59,29 @@ connection.close()
 # for c in cursor:
 #     print(c)
 
+# 전체 데이터 조회
+sql = "select count(*) from test"
+cursor.execute(sql)
+count = cursor.fetchone()
+print(count[0])
+print(type(count))
 
+#데이터 수정
+# sql = "update test set pw='123' where id = 'test1'"
+# cursor.execute(sql)
+# cursor.close
+# connection.commit()
+# connection.close
 
+# id = input("아이디 입력")
+# new_pw = input("새 비밀번호 입력 : ")
+#
+# sql = "update test set pw=:1 where id =:2"
+# data = (new_pw,id)
+# cursor.execute(sql,data)
+# cursor.close()
+# connection.commit()
+# connection.close
 
 
 
