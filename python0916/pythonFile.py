0369b62d630bf976256347a41ce4b7369df3fe4b
yskim-web <abcm556@naver.com>
2020-09-17 09:44:05 +0900

    #

--- python0916/pythonFile.py
+++ python0916/pythonFile.py
@@ -7,10 +7,13 @@
 '''
 import cx_Oracle
 
+# cx_Oracle.init_oracle_client(lib_dir='c:\oracle\instantclient_11_2')
 connection = cx_Oracle.connect("java06/java06@nullmaster.iptime.org:3000/orcl")
 print('{}'.format(connection.version))
 
-# cursor = connection.cursor()
-# cursor.execute("select * from test")
-# for c in cursor:
-#     print(c)
+cursor = connection.cursor()
+cursor.execute("select * from test")
+for c in cursor:
+    print(c)
+
+
